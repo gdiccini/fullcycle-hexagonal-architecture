3bc2e5a0889e707a0d650d8269a93c861f34be0d
Giovani Diccini <giovani.diccini@gmail.com>
2023-07-16 12:43:47 -0300

    add product Enable test

--- application/product.go
+++ application/product.go
@@ -1,5 +1,7 @@
 package application
 
+import "errors"
+
 type ProductInterface interface {
 	IsValid() (bool, error)
 	Enable() error
@@ -23,20 +25,31 @@ type Product struct {
 	Status string
 }
 
-func (p *Product) IsValid() (bool, error) {
-
-}
+// func (p *Product) IsValid() (bool, error) {}
 
 func (p *Product) Enable() error {
+	if p.Price > 0 {
+		p.Status = ENABLED
+		return nil
+	}
 
+	return errors.New("The price must be greater than zero to enable the product")
 }
 
-func (p *Product) Disable() error {}
+// func (p *Product) Disable() error {}
 
-func (p *Product) GetID() string {}
+func (p *Product) GetID() string {
+	return p.ID
+}
 
-func (p *Product) GetName() string {}
+func (p *Product) GetName() string {
+	return p.Name
+}
 
-func (p *Product) GetStatus() string {}
+func (p *Product) GetStatus() string {
+	return p.Status
+}
 
-func (p *Product) GetPrice() float32 {}
+func (p *Product) GetPrice() float32 {
+	return p.Price
+}

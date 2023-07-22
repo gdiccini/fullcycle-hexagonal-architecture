9e14da85abf1e89ab98e01fa69d03ecbb846bfd7
Giovani Diccini <giovani.diccini@gmail.com>
2023-07-22 07:20:44 -0300

    add main

--- main.go
+++ main.go
@@ -12,9 +12,7 @@ func main() {
 	db, _ := sql.Open("sqlite3", "db.sqlite")
 	productDbAdapter := db2.NewProductDb(db)
 	productService := application.NewProductService(productDbAdapter)
-	productService.Create("Product Example", 30)
+	product, _ := productService.Create("Product Example", 30)
 
-
-
-
+	productService.Enable(product)
 }

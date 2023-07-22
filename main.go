10d7fb605104a1b2663569f08c78f3d0927f988a
Giovani Diccini <giovani.diccini@gmail.com>
2023-07-22 09:47:36 -0300

    add cobra cli

--- main.go
+++ main.go
@@ -1,18 +1,11 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"database/sql"
-
-	db2 "github.com/codeedu/go-hexagonal/adapters/db"
-	"github.com/codeedu/go-hexagonal/application"
-	_ "github.com/mattn/go-sqlite3"
-)
+import "github.com/codeedu/go-hexagonal/cmd"
 
 func main() {
-	db, _ := sql.Open("sqlite3", "db.sqlite")
-	productDbAdapter := db2.NewProductDb(db)
-	productService := application.NewProductService(productDbAdapter)
-	product, _ := productService.Create("Product Example", 30)
-
-	productService.Enable(product)
+	cmd.Execute()
 }
